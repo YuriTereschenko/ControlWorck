92f9ade6562a14a8fda3d09374d424f7dbd575a5
Юрий Терещенко <yuri.tereschenko@icloud.com>
2022-05-04 23:29:58 +0500

    Добавлен метод для создания нового массива и метод для вывода массива в консоль

--- Program.cs
+++ Program.cs
@@ -1,7 +1,12 @@
 ﻿//Написать программу, которая из имеющегося массива строк формирует массив строк, длинна которых меньше либо равна 3 символа.
 
 string[] arrayOfStrings = { "Привет", "мир", "$)", "дом", "корабль", "морж" };
-System.Console.WriteLine(NewArrayLength(arrayOfStrings));
+
+int length = NewArrayLength(arrayOfStrings);
+string[] Decision = PickRelevantWords(length, arrayOfStrings);
+PrintArray(arrayOfStrings, "Имеющийся массив");
+PrintArray(Decision, "Новый массив");
+
 
 int NewArrayLength(string[] array)
 {
@@ -14,4 +19,32 @@ int NewArrayLength(string[] array)
         }
     }
     return count;
+}
+
+
+string[] PickRelevantWords(int length, string[] array)
+{
+    string[] Result = new string[length];
+    int j = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length < 4)
+        {
+            Result[j] = array[i];
+            j++;
+        }
+    }
+    return Result;
+}
+
+
+void PrintArray(string[] array, string nameOfArray)
+{
+    System.Console.WriteLine(nameOfArray + ":");
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        System.Console.Write($"{array[i]}, ");
+    }
+    System.Console.WriteLine(array[array.Length - 1]);
+    System.Console.WriteLine("---------");
 }
