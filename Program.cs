e75ecbac912bb41490fc166633674eaffc987bc7
Юрий Терещенко <yuri.tereschenko@icloud.com>
2022-05-04 23:15:25 +0500

    добавлен метод определяющий длину нового массива

--- Program.cs
+++ Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿//Написать программу, которая из имеющегося массива строк формирует массив строк, длинна которых меньше либо равна 3 символа.
+
+string[] arrayOfStrings = { "Привет", "мир", "$)", "дом", "корабль", "морж" };
+System.Console.WriteLine(NewArrayLength(arrayOfStrings));
+
+int NewArrayLength(string[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length < 4)
+        {
+            count++;
+        }
+    }
+    return count;
+}
